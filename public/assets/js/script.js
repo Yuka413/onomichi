73871c9585c5ee592286c7a42234093e95b5fd89
Yuka413 <siba0214koro@gmail.com>
2025-02-07 01:34:06 +0900

    応募の仕方セクション完成

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,3 +8,38 @@ $(".l-header__open").on("click", function () {
     $(".l-header__open-bar").addClass("is-active");
   }
 });
+
+// スワイパー
+const swiper = new Swiper(".p-onomichi__swiper", {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  speed: 6000,
+  followFinger: false,
+  // autoplay: {
+  //   delay: 0,
+  // },
+  breakpoints: {
+    768: {
+      spaceBetween: 20,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
