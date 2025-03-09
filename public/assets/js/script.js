b3f293d4d4fa113d723d4e91dca08be33519d5fe
Yuka413 <siba0214koro@gmail.com>
2025-03-09 11:31:10 +0900

    Q&Aセクション完成

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -24,7 +24,6 @@ const swiper = new Swiper(".p-onomichi__swiper", {
   breakpoints: {
     768: {
       spaceBetween: 20,
-
     },
   },
 
@@ -52,13 +51,13 @@ const spotSwiper = new Swiper(".p-spots__swiper", {
   centeredSlides: true,
   slidesPerView: "auto",
   allowTouchMove: true,
-
+  freeMode: true,
 
   breakpoints: {
     768: {
       spaceBetween: 32,
       slidesPerView: "auto",
-
+      centeredSlides: false,
     },
   },
 
@@ -67,4 +66,49 @@ const spotSwiper = new Swiper(".p-spots__swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// 自作ボタンクリックでスワイプ
+const $prevButton = $("#js-prevButton");
+const $nextButton = $("#js-nextButton");
+
+$prevButton.on("click", function () {
+  spotSwiper.slidePrev();
+});
+$nextButton.on("click", function () {
+  spotSwiper.slideNext();
+});
+
+// modal
+const modalItems = document.querySelectorAll(".c-benefit");
+console.log(modalItems);
+modalItems.forEach((item) => {
+  const dialog = item.querySelector(".c-dialog");
+  if (dialog) {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      dialog.showModal();
+    });
+    const closeButton = dialog.querySelector(".c-dialog__button");
+    if (closeButton) {
+      closeButton.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("閉じる");
+        dialog.close();
+      });
+    }
+  }
+});
+
+// アコーディオン
+$(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+  if ($(this).parent().hasClass("is-open")) {
+    $(this).parent().removeClass("is-open");
+    $(this).next().slideUp();
+  } else {
+    $(this).parent().addClass("is-open");
+    $(this).next().slideDown();
+  }
 });
